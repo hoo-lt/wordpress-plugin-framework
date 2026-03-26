c5084df5ca4107eac6af3b74e99405f92cda86e6
Viacheslav Ravdin <viacheslav@hoo.lt>
2026-03-26 19:11:03 +0200

    Database class modified to return json

--- src/Database/Database.php
+++ src/Database/Database.php
@@ -9,15 +9,41 @@ class Database implements DatabaseInterface
 	public function __construct(
 		protected readonly wpdb $wpdb
 	) {
+		$this->wpdb->query(
+			"SET SESSION group_concat_max_len = 1048576"
+		);
 	}
 
 	public function select(Query\Select\QueryInterface $query): array
 	{
 		$results = $this->wpdb->get_results($query(), ARRAY_A);
-		if (!$results) {
+		if ($this->wpdb->last_error) {
 			throw new DatabaseException('invalid query');
 		}
 
+		if ($results === null) {
+			return [];
+		}
+
 		return $results;
+	}
+
+	public function json(Query\Select\QueryInterface $query): array
+	{
+		$var = $this->wpdb->get_var($query());
+		if ($this->wpdb->last_error) {
+			throw new DatabaseException('invalid query');
+		}
+
+		if ($var === null) {
+			return [];
+		}
+
+		$json = json_decode($var, true);
+		if ($json === null) {
+			throw new DatabaseException('error decoding json');
+		}
+
+		return $json;
 	}
 }
--- src/Database/DatabaseInterface.php
+++ src/Database/DatabaseInterface.php
@@ -5,4 +5,5 @@ namespace Hoo\WordPressPluginFramework\Database;
 interface DatabaseInterface
 {
 	public function select(Query\Select\QueryInterface $query): array;
+	public function json(Query\Select\QueryInterface $query): array;
 }
