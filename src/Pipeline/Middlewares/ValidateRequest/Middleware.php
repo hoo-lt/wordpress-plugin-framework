57edcc61ee1df029f5abccf119b618ec16f03f7d
Viacheslav Ravdin <viacheslav@hoo.lt>
2026-04-19 01:15:52 +0300

    readibility fixes

--- src/Pipeline/Middlewares/ValidateRequest/Middleware.php
+++ src/Pipeline/Middlewares/ValidateRequest/Middleware.php
@@ -2,11 +2,13 @@
 
 namespace Hoo\WordPressPluginFramework\Pipeline\Middlewares\ValidateRequest;
 
-use Hoo\WordPressPluginFramework\Pipeline\Middlewares\MiddlewareInterface;
-use Hoo\WordPressPluginFramework\Pipeline\Middlewares\MiddlewareTrait;
 use Hoo\WordPressPluginFramework\Http\Request\RequestInterface;
-use Hoo\WordPressPluginFramework\Pipeline\Middlewares\ValidateRequest\Input\InputInterface;
-use Hoo\WordPressPluginFramework\Pipeline\Middlewares\ValidateRequest\Rules\RuleInterface;
+use Hoo\WordPressPluginFramework\Pipeline\Middlewares\{
+	MiddlewareInterface,
+	MiddlewareTrait,
+	ValidateRequest\Input\InputInterface,
+	ValidateRequest\Rules\RuleInterface,
+};
 
 readonly class Middleware implements MiddlewareInterface
 {
@@ -15,57 +17,91 @@ readonly class Middleware implements MiddlewareInterface
 	public function __construct(
 		protected RequestInterface $request,
 		protected array $inputs = [],
-		protected ?InputInterface $currentInput = null,
+		protected ?InputInterface $input = null,
 	) {
 	}
 
 	public function post(string $name): self
 	{
-		return new self($this->request, $this->resolvedInputs(), new Input\Post($this->request, $name));
+		return new self(
+			$this->request,
+			$this->inputs(),
+			new Input\Post(
+				$this->request,
+				$name
+			)
+		);
 	}
 
 	public function get(string $name): self
 	{
-		return new self($this->request, $this->resolvedInputs(), new Input\Get($this->request, $name));
+		return new self(
+			$this->request,
+			$this->inputs(),
+			new Input\Get(
+				$this->request,
+				$name
+			)
+		);
 	}
 
 	public function int(): self
 	{
-		return new self($this->request, $this->inputs, $this->currentInput->withRule(new Rules\Int\Rule()));
+		return new self(
+			$this->request,
+			$this->inputs,
+			$this->input->withRule(
+				new Rules\Int\Rule()
+			)
+		);
 	}
 
 	public function float(): self
 	{
-		return new self($this->request, $this->inputs, $this->currentInput->withRule(new Rules\Float\Rule()));
+		return new self(
+			$this->request,
+			$this->inputs,
+			$this->input->withRule(
+				new Rules\Float\Rule()
+			)
+		);
 	}
 
 	public function string(): self
 	{
-		return new self($this->request, $this->inputs, $this->currentInput->withRule(new Rules\String\Rule()));
-	}
-
-	public function in(array $allowed): self
-	{
-		return new self($this->request, $this->inputs, $this->currentInput->withRule(new Rules\InArray\Rule($allowed)));
+		return new self(
+			$this->request,
+			$this->inputs,
+			$this->input->withRule(
+				new Rules\String\Rule()
+			)
+		);
 	}
 
 	public function rule(RuleInterface $rule): self
 	{
-		return new self($this->request, $this->inputs, $this->currentInput->withRule($rule));
+		return new self(
+			$this->request,
+			$this->inputs,
+			$this->input->withRule(
+				$rule
+			)
+		);
 	}
 
-	protected function resolvedInputs(): array
+	protected function inputs(): array
 	{
-		return $this->currentInput
-			? [...$this->inputs, $this->currentInput]
-			: $this->inputs;
+		return $this->input ? [
+			...$this->inputs,
+			$this->input
+		] : $this->inputs;
 	}
 
 	public function __invoke(callable $callable): mixed
 	{
 		$errors = [];
 
-		foreach ($this->resolvedInputs() as $input) {
+		foreach ($this->inputs() as $input) {
 			foreach ($input->rules() as $rule) {
 				if (!$rule($input->value())) {
 					$errors[] = "{$input->name()} {$rule->error()}";
