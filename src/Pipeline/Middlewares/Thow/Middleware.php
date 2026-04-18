e00961bf2379e149d2288da1bb5e734a267de68b
Viacheslav Ravdin <viacheslav@hoo.lt>
2026-04-19 00:54:35 +0300

    adding middleware for testing

---
+++ src/Pipeline/Middlewares/Thow/Middleware.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Hoo\WordPressPluginFramework\Pipeline\Middlewares\Thow;
+
+use Hoo\WordPressPluginFramework\Pipeline\Middlewares\MiddlewareException;
+use Hoo\WordPressPluginFramework\Pipeline\Middlewares\MiddlewareInterface;
+use Hoo\WordPressPluginFramework\Pipeline\Middlewares\MiddlewareTrait;
+
+readonly class Middleware implements MiddlewareInterface
+{
+	use MiddlewareTrait;
+
+	public function __invoke(callable $callable): mixed
+	{
+		throw new MiddlewareException('just throwing to test pipeline');
+	}
+}
