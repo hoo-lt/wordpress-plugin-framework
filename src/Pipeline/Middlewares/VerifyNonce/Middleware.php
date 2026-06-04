bdd2ccb86bfa3cfe6b780125b47a8535a72cafd8
Viacheslav Ravdin <viacheslav@hoo.lt>
2026-06-04 04:14:41 +0300

    cosmetics

--- src/Pipeline/Middlewares/VerifyNonce/Middleware.php
+++ src/Pipeline/Middlewares/VerifyNonce/Middleware.php
@@ -27,6 +27,11 @@ readonly class Middleware implements MiddlewareInterface
 		return new static($this->name, $action);
 	}
 
+	public function withoutAction(): static
+	{
+		return new static($this->name, -1);
+	}
+
 	public function __invoke(RequestInterface $request, Closure $closure): mixed
 	{
 		if ($this->name === null) {
@@ -38,7 +43,7 @@ readonly class Middleware implements MiddlewareInterface
 			throw new ForbiddenException('nonce is not presented', 'verify_nonce_error');
 		}
 
-		if (!wp_verify_nonce($nonce, $this->action)) {
+		if (wp_verify_nonce($nonce, $this->action) === false) {
 			throw new ForbiddenException('error verifying nonce', 'verify_nonce_error');
 		}
 
--- src/Pipeline/Middlewares/VerifyNonce/MiddlewareInterface.php
+++ src/Pipeline/Middlewares/VerifyNonce/MiddlewareInterface.php
@@ -7,5 +7,7 @@ use Hoo\WordPressPluginFramework\Pipeline\Middlewares;
 interface MiddlewareInterface extends Middlewares\MiddlewareInterface
 {
 	public function withName(string $name): static;
+
 	public function withAction(string|int $action): static;
+	public function withoutAction(): static;
 }
