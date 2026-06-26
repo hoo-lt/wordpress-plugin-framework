39345df0edf1b789e18fdffc82f290a6f9200946
Viacheslav Ravdin <viacheslav@hoo.lt>
2026-06-26 16:16:45 +0300

    sugar added

--- src/Http/Client/Client.php
+++ src/Http/Client/Client.php
@@ -4,6 +4,7 @@ namespace Hoo\WordPressPluginFramework\Http\Client;
 
 use Hoo\WordPressPluginFramework\{
 	Http\Client\Request\RequestInterface,
+	Http\Client\Request\RequestFactoryInterface,
 	Http\Client\Response\ResponseInterface,
 	Http\Client\Response\ResponseFactoryInterface,
 };
@@ -12,8 +13,58 @@ use WP_Error;
 readonly class Client implements ClientInterface
 {
 	public function __construct(
+		protected RequestFactoryInterface $requestFactory,
 		protected ResponseFactoryInterface $responseFactory,
 	) {
+	}
+
+	public function get(string $url, ?array $headers = null, array|string|null $body = null): ResponseInterface
+	{
+		return $this->request(
+			$this->requestFactory->create('GET', $url, $headers, $body),
+		);
+	}
+
+	public function head(string $url, ?array $headers = null, array|string|null $body = null): ResponseInterface
+	{
+		return $this->request(
+			$this->requestFactory->create('HEAD', $url, $headers, $body),
+		);
+	}
+
+	public function post(string $url, ?array $headers = null, array|string|null $body = null): ResponseInterface
+	{
+		return $this->request(
+			$this->requestFactory->create('POST', $url, $headers, $body),
+		);
+	}
+
+	public function put(string $url, ?array $headers = null, array|string|null $body = null): ResponseInterface
+	{
+		return $this->request(
+			$this->requestFactory->create('PUT', $url, $headers, $body),
+		);
+	}
+
+	public function patch(string $url, ?array $headers = null, array|string|null $body = null): ResponseInterface
+	{
+		return $this->request(
+			$this->requestFactory->create('PATCH', $url, $headers, $body),
+		);
+	}
+
+	public function delete(string $url, ?array $headers = null, array|string|null $body = null): ResponseInterface
+	{
+		return $this->request(
+			$this->requestFactory->create('DELETE', $url, $headers, $body),
+		);
+	}
+
+	public function options(string $url, ?array $headers = null, array|string|null $body = null): ResponseInterface
+	{
+		return $this->request(
+			$this->requestFactory->create('OPTIONS', $url, $headers, $body),
+		);
 	}
 
 	public function request(RequestInterface $request): ResponseInterface
--- src/Http/Client/ClientInterface.php
+++ src/Http/Client/ClientInterface.php
@@ -9,5 +9,13 @@ use Hoo\WordPressPluginFramework\{
 
 interface ClientInterface
 {
+	public function get(string $url, ?array $headers = null, array|string|null $body = null): ResponseInterface;
+	public function head(string $url, ?array $headers = null, array|string|null $body = null): ResponseInterface;
+	public function post(string $url, ?array $headers = null, array|string|null $body = null): ResponseInterface;
+	public function put(string $url, ?array $headers = null, array|string|null $body = null): ResponseInterface;
+	public function patch(string $url, ?array $headers = null, array|string|null $body = null): ResponseInterface;
+	public function delete(string $url, ?array $headers = null, array|string|null $body = null): ResponseInterface;
+	public function options(string $url, ?array $headers = null, array|string|null $body = null): ResponseInterface;
+
 	public function request(RequestInterface $request): ResponseInterface;
 }
