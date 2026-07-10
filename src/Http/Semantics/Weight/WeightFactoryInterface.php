4791a5510f55fb1eeddd4b940e997a90f9f0ce71
Viacheslav Ravdin <viacheslav@hoo.lt>
2026-07-11 00:02:19 +0300

    tests should fail

--- src/Http/Semantics/Weight/WeightFactoryInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Hoo\WordPressPluginFramework\Http\Semantics\Weight;
-
-interface WeightFactoryInterface
-{
-	public function create(string $mediaRange): ?WeightInterface;
-}
--- src/Http/Semantics/Weight/WeightInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Hoo\WordPressPluginFramework\Http\Semantics\Weight;
-
-interface WeightInterface
-{
-	public function value(): float;
-}
