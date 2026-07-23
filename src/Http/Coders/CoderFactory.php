7d768a9ae50c258e833d054fa24ed13a253c93a1
Viacheslav Ravdin <viacheslav@hoo.lt>
2026-07-24 02:07:59 +0300

    coder factory simplified

--- src/Http/Coders/CoderFactory.php
+++ src/Http/Coders/CoderFactory.php
@@ -13,49 +13,29 @@ readonly class CoderFactory implements CoderFactoryInterface
 
 	public function createDecoder(mixed $encoded, MediaTypeInterface $mediaType): CoderInterface
 	{
-		$coder = $this->tryCreateDecoder($encoded, $mediaType);
-		if ($coder === null) {
-			throw new CoderFactoryException('no coder decodes this media type');
-		}
-
-		return $coder;
-	}
-
-	public function tryCreateDecoder(mixed $encoded, MediaTypeInterface $mediaType): ?CoderInterface
-	{
 		foreach ($this->coders as $coder) {
 			if (
-				$coder->codes($mediaType) &&
-				$coder->decodes($encoded)
+				$coder->decodes($encoded) &&
+				$coder->codes($mediaType)
 			) {
 				return $coder;
 			}
 		}
 
-		return null;
+		throw new CoderFactoryException('no coder decodes this media type');
 	}
 
 	public function createEncoder(mixed $decoded, MediaTypeInterface $mediaType): CoderInterface
 	{
-		$coder = $this->tryCreateEncoder($decoded, $mediaType);
-		if ($coder === null) {
-			throw new CoderFactoryException('no coder encodes this value for this media type');
-		}
-
-		return $coder;
-	}
-
-	public function tryCreateEncoder(mixed $decoded, MediaTypeInterface $mediaType): ?CoderInterface
-	{
 		foreach ($this->coders as $coder) {
 			if (
-				$coder->codes($mediaType) &&
-				$coder->encodes($decoded)
+				$coder->encodes($decoded) &&
+				$coder->codes($mediaType)
 			) {
 				return $coder;
 			}
 		}
 
-		return null;
+		throw new CoderFactoryException('no coder encodes this value for this media type');
 	}
 }
--- src/Http/Coders/CoderFactoryInterface.php
+++ src/Http/Coders/CoderFactoryInterface.php
@@ -7,8 +7,5 @@ use Hoo\WordPressPluginFramework\Http\Semantics\ContentType\MediaType\MediaTypeI
 interface CoderFactoryInterface
 {
 	public function createDecoder(mixed $encoded, MediaTypeInterface $mediaType): CoderInterface;
-	public function tryCreateDecoder(mixed $encoded, MediaTypeInterface $mediaType): ?CoderInterface;
-
 	public function createEncoder(mixed $decoded, MediaTypeInterface $mediaType): CoderInterface;
-	public function tryCreateEncoder(mixed $decoded, MediaTypeInterface $mediaType): ?CoderInterface;
 }
